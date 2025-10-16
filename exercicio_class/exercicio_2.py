b58de1e2bfeebc17ad62d7007d1530211a83c55d
FELIPE POTRAFKE RODRIGUES <24099>
2025-10-16 12:19:44 -0300

    exercicios resolvidos2!

--- exercicio_class/exercicio_2.py
+++ exercicio_class/exercicio_2.py
@@ -2,16 +2,12 @@ class Retangulo:
     def __init__ (self, largura = 0, altura = 0, area = 0, perimetro = 0):
         self.altura = int(input("Qual é a altura do retangulo? \n"))
         self.largura = int(input("Qual é a largura do retangulo? \n"))
-        self.area = area
-        self.perimetro = perimetro
 
     def calcularArea(self):
-        self.area = self.largura * self.altura
-        print(f"A área do retangulo é: {self.area}")
+        print(f"A área do retangulo é: {self.largura * self.altura}")
 
     def calcularPerimetro(self):
-        self.perimetro = 2 * (self.altura + self.largura)
-        print(f"O perímetro do retângulo é: {self.perimetro}")
+        print(f"O perímetro do retângulo é: {2 * (self.altura + self.largura)}")
 
 
 retangulo = Retangulo()
